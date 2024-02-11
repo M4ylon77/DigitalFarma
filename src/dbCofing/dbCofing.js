0784373ed3d8a3c28148a4674889159e38c2c372
M4ylon77 <rafaelmendesmaylon77@gmail.com>
2024-02-11 18:32:08 -0300

    first commit

--- src/dbCofing/dbCofing.js
+++ src/dbCofing/dbCofing.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 
 //const configdb =  "mongodb+srv://Maylon77:153987@cluster0.rh84gdu.mongodb.net/farma?retryWrites=true&w=majority";
 
-const dbUser = process.env.DB_USER;
-const dbPassWord = process.env.DB_PASS;
+const configDb = process.env.MONGODB_CONNECT_URI;
+
 
  
 
 const conexao = () => {
     
-    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassWord}@cluster0.rh84gdu.mongodb.net/DigitaFarma?retryWrites=true&w=majority`);
+    mongoose.connect(configDb);
     
 mongoose.connection.on("error", ()=>{
     console.error("erro ao conectar com o mongoDB")
